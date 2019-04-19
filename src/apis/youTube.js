b4d9ac312817401051fa33665b3746a779f67b47
chintansbhut <bhut.chintan@gmail.com>
2019-04-19 16:31:30 +0530

    Update youTube.js

--- src/apis/youTube.js
+++ src/apis/youTube.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyCYq7uZfrOnH5aItlaBGZnAFz3bhHqG5_M";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
